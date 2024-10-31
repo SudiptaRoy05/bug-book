ea686f450188ebf4baad833be661b7657b3b68b7
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-10-31 23:14:48 +0600

    added navbar and customize as needed

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,9 @@
+
+export default function Error() {
+  return (
+    <div>
+        <h2 className="text-5xl">Page not found</h2>
+        <p>status : 4o4</p>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './components/Root'
+import Root from './components/Root/Root'
+import Error from './components/Error/Error'
+import Home from './components/Home/Home'
+import Dashboard from './components/Dashboard/Dashboard'
 
 
 const router = createBrowserRouter([
   {
     path:'/',
     element:<Root></Root>,
+    errorElement:<Error></Error>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'/dashboard',
+        element:<Dashboard></Dashboard>
+      },
+    ]
   },
 ])
 createRoot(document.getElementById('root')).render(
